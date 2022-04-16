3f75ee5f44dc0531f4911edb3120194c6cf27bad
Collins Sirwani <sircollo71@gmail.com>
2022-04-16 22:52:09 +0300

    Give search functionality, with search function

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { User } from '../classes/user';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  @Output()
+  searchData = new EventEmitter<any>()
 
+  userProfile = new User("","",0,0)
+  searchUserName: any
+
+  search(){
+    this.searchData.emit(this.searchUserName);
+    this.userProfile = new User("","",0,0)
+    this.searchUserName = ""  //clear form field
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../classes/user';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  users!:User
 
   constructor() { }
 
