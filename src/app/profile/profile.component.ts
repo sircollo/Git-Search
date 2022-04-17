7f5059bbcbf071f132ddda1a2b8105f7b6077440
Collins Sirwani <sircollo71@gmail.com>
2022-04-18 00:20:12 +0300

    Add empty string to clear search field

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { GitSearchService } from '../services/git-search.service';
 import { Observable } from 'rxjs';
 @Component({
@@ -7,12 +7,14 @@ import { Observable } from 'rxjs';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+
+  @Output() searchOutput = new EventEmitter<any>()
   // profile:any = []
   profile:any = [];
   repos!:any[]
 
   // userName!:string
-  userName:string = 'sircollo'
+  userName:string = ''
   constructor(private gitSearchService:GitSearchService) {
     // this.gitSearchService.getProfileInfo().subscribe((profile)=>{
     //   this.profile = profile;
@@ -30,12 +32,14 @@ export class ProfileComponent implements OnInit {
     this.gitSearchService.getProfileInfo().subscribe((profile)=>{
       this.profile = profile;
       console.log(profile)
+      this.userName=""
     });
   
    this.gitSearchService.getProfileRepo().subscribe((repos:any)=>{
      console.log(repos)
      this.repos = repos
    })
+   
    
   }
 
