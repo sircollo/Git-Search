915ec666245716378f7e318f7d81c06b3c160e4d
Collins Sirwani <sircollo71@gmail.com>
2022-04-18 00:07:03 +0300

    Adds repositories class and constructor function with parameters

---
+++ src/app/classes/repositories.ts
@@ -0,0 +1,10 @@
+export class Repositories {
+  constructor(
+      public name:string,
+      public html_url:string,
+      public description:string,
+      public language:string,
+      public created_at: Date){
+        
+      }
+}
